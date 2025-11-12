ef972879a65e385941cffd20270cff2309c01132
Adrian Escalante <adres@MacBook-Pro-de-Adrian.local>
2025-11-12 15:58:58 -0600

    moving API_KEY to env variables

---
+++ src/middlewares/verifyApiKey.js
@@ -0,0 +1,14 @@
+// TODO - Move API key to environment variable
+export const API_KEY = process.env.API_KEY
+
+export default function verifyApiKey(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    const [, key] = req.headers.authorization.split(' ');
+    if (key && key === API_KEY) {
+        next();
+    } else {
+        res.status(403).json({ error: "Forbidden" });
+    }
+}
--- src/server.js
+++ src/server.js
@@ -2,27 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import catalogRoutes from './routes/catalog.routes.js';
-
-// TODO - Move API key to environment variable
-const API_KEY = '12345abcde';
+import verifyApiKey from './middlewares/verifyApiKey.js';
 
 const app = express();
 
 const corsOptions = {
   origin: 'http://localhost:5173',
 };
-
-function verifyApiKey(req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-  const [, key] = req.headers.authorization.split(' ');
-  if (key && key === API_KEY) {
-    next();
-  } else {
-    res.status(403).json({ error: "Forbidden" });
-  }
-}
 
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
